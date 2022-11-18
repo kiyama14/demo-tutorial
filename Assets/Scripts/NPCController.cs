312279ffed0bf8e56be763e33858040884432299
Mauricio Kiyama <kiyama14@gmail.com>
2022-11-18 08:12:19 -0700

    Added new character sprites and implemented the challenger

--- Assets/Scripts/NPCController.cs
+++ Assets/Scripts/NPCController.cs
@@ -6,8 +6,22 @@ public class NPCController : MonoBehaviour , Interactable
 {
     [SerializeField] Dialog dialog;
 
+
     public void Interact()
     {
         StartCoroutine(DialogManager.Instance.ShowDialog(dialog));
+    }
+
+    public void LookTowards(Vector3 targerPos)
+    {
+        var xdiff = Mathf.Floor(targerPos.x) - Mathf.Floor(transform.position.x);
+        var ydiff = Mathf.Floor(targerPos.y) - Mathf.Floor(transform.position.y);
+
+        if (xdiff == 0 || ydiff == 0)
+        {
+
+        }
+        else
+            Debug.LogError("Cannot look diagonal!");
     }
 }
